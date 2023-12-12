1b69435a458b58a29d6ce25e60a7a679df358a8c
Gilangg13 <gilangputra295@gmail.com>
2023-12-12 23:54:56 +0700

    Add a route for search and loading indicator

--- src/scripts/views/app.js
+++ src/scripts/views/app.js
@@ -4,6 +4,7 @@
 import routes from "../routes/routes";
 import UrlParser from "../routes/url-parser";
 import DrawerInitiator from "../utils/drawer-initiator";
+import SearchResult from "./pages/search-result";
 
 class App {
   constructor({ content, menuBtn, searchBtn, cancelBtn, items, form }) {
@@ -26,6 +27,7 @@ class App {
       cancelBtn: this._cancelBtn,
       items: this._items,
       form: this._form,
+      searchHandler: this._searchHandler.bind(this),
     });
   }
 
@@ -34,27 +36,59 @@ class App {
     const page = routes[url];
 
     if (page) {
-      //   this._showLoading();
+      this._showLoading();
       try {
-        // await this._wait(500);
+        await this._wait(500);
         this._content.innerHTML = await page.render();
         await page.afterRender();
       } catch (error) {
         console.log("Failed to render page", error);
       }
-      //   this._hideLoading();
+      this._hideLoading();
     }
 
-    // const skipLinkElem = document.querySelector(".skip-to-content");
-    // skipLinkElem.addEventListener("click", (event) => {
-    //   event.preventDefault();
-    //   document.querySelector("#mainContent").focus();
-    // });
+    // untuk route pencarian
+    const searchRoute = url.resource === "search";
+    if (searchRoute) {
+      try {
+        this._content.innerHTML = await SearchResult.render();
+        await SearchResult.afterRender(url.id || "");
+      } catch (error) {
+        console.error(`Gagal merender hasil pencarian: ${error}`);
+      }
+    }
+
+    const skipLinkElem = document.querySelector(".skip-to-content");
+    skipLinkElem.addEventListener("click", (event) => {
+      event.preventDefault();
+      document.querySelector("#mainContent").focus();
+    });
   }
 
-  // _wait(ms) {
-  //   return new Promise((resolve) => setTimeout(resolve, ms));
-  // }
+  _searchHandler(query) {
+    window.location.hash = `/search?q=${query}`;
+    this.renderPage();
+  }
+
+  _wait(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  _showLoading() {
+    this._content.innerHTML = `
+      <div class="loading-indicator">
+        <div class="loading-indicator-circle"></div>
+        <span class="loading-indicator-text">Loading...</span>
+      </div>
+    `;
+  }
+
+  _hideLoading() {
+    const loadingIndicator = this._content.querySelector(".loading-indicator");
+    if (loadingIndicator) {
+      loadingIndicator.remove();
+    }
+  }
 }
 
 export default App;
