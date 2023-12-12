f6266627b53412e8611c04bfe7bc5af19c9833b5
Gilangg13 <gilangputra295@gmail.com>
2023-12-12 23:42:25 +0700

    change the flow and logic routes and url

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,14 +1,18 @@
 import Home from "../views/pages/home";
+import Favorite from "../views/pages/favorite-museum";
 import DetailMuseum from "../views/pages/detail";
-// import Favorite from "../views/pages/favorite-museum";
 import AboutUs from "../views/pages/about";
+import MuseumCategory from "../views/pages/museum-category";
+import SearchResult from "../views/pages/search-result";
 
 const routes = {
   "/": Home,
   "/home": Home,
-  "/museum/:id": DetailMuseum,
-  // "/favorite": Favorite,
+  "/favorite": Favorite,
   "/about": AboutUs,
+  "/detail/:id": DetailMuseum,
+  "/provinsi/:id": MuseumCategory,
+  "/search?q=:id": SearchResult,
 };
 
 export default routes;
--- src/scripts/routes/url-parser.js
+++ src/scripts/routes/url-parser.js
@@ -1,26 +1,41 @@
+/* eslint-disable prefer-const */
+/* eslint-disable no-dupe-else-if */
+/* eslint-disable no-else-return */
+/* eslint-disable object-curly-newline */
 /* eslint-disable operator-linebreak */
+
 const UrlParser = {
   parseActiveUrlWithCombiner() {
     const url = window.location.hash.slice(1).toLowerCase();
     const splittedUrl = this._urlSplitter(url);
+    // console.log("Parsed URL:", url);
+    // console.log("Splitted URL:", splittedUrl);
     return this._urlCombiner(splittedUrl);
   },
 
   parseActiveUrlWithoutCombiner() {
     const url = window.location.hash.slice(1).toLowerCase();
+    // console.log("Parsed URL Without Combiner:", url);
     return this._urlSplitter(url);
   },
 
   _urlSplitter(url) {
-    const urlSplits = url.split("/");
+    const urlSplits = url.split("?");
+    const queryParams = new URLSearchParams(urlSplits[1] || "");
+    const pathSegments = urlSplits[0].split("/");
+    const resource = pathSegments[1] || null;
+    const isSearch = resource === "search";
     return {
-      resource: urlSplits[1] || null,
-      id: urlSplits[2] || null,
+      resource: isSearch ? "search" : resource,
+      id: isSearch ? queryParams.get("q") || null : pathSegments[2] || null,
       verb: urlSplits[3] || null,
     };
   },
 
   _urlCombiner({ resource, id, verb }) {
+    if (resource === "search") {
+      return (resource ? `/${resource}` : "") + (id ? `?q=:id` : "");
+    }
     return (
       (resource ? `/${resource}` : "/") +
       (id ? "/:id" : "") +
