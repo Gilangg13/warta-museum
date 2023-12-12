8de6fbbbb8816ba450fdeca72dd78e1c2cf9b542
Gilangg13 <gilangputra295@gmail.com>
2023-12-12 23:32:28 +0700

    added see all button and improved display

--- src/scripts/views/pages/home.js
+++ src/scripts/views/pages/home.js
@@ -1,78 +1,69 @@
+/* eslint-disable max-len */
 /* eslint-disable function-paren-newline */
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-await-in-loop */
 /* eslint-disable no-restricted-syntax */
 import MuseumSource from "../../data/museum-resource";
-
 import { createMuseumItemTemplate } from "../templates/template-creator";
 
 const Home = {
   async render() {
-    const provinsis = [
-      "DKI Jakarta",
-      "Jawa Barat",
-      "Jawa Tengah",
-      "Jawa Timur",
-      "Banten",
-    ];
+    const provinsiList = await MuseumSource.provinsiList();
+    console.log(provinsiList);
 
     return `
         <section class="hero-banner text-light py-5">
-            <!-- <div class="container"> -->
-            <div class="row row align-items-center">
-                <div class="col-lg-6 order-lg-1">
+            <div class="row align-items-center">
+                <div class="col-lg-6 order-lg-1 d-flex justify-content-center">
                     <img src="./images/hero.png" class="img-fluid" alt="Web Development">
                 </div>
                 <div class="col-lg-6">
                     <h1 class="mt-5">Eksplorasi Budaya, Museum sebagai Tempat Penemuan Tanpa Batas.</h1>
                     <p class="lead text-light my-4">Eksplorasi Tanpa Batas, Belajar Tanpa Henti: Museum Virtual Anda.</p>
-                    <a href="#" class="btn btn-outline-secondary btn-lg border">Explore Now</a>
+                    <a href="#content" class="btn btn-outline-secondary btn-lg border">Explore Now</a>
                 </div>
             </div>
-            <!-- </div> -->
         </section>
-
 
         <section id="content">
             <div class="content-heading text-center">
                 <h2>Temukan Museum Favoritmu</h2>
-                <p>Menelusuri Jejak Budaya: Eksplorasi Sejarah, Nikmati Karya Seni, dan Temukan Inspirasi di Setiap Sudut
-                Museum.</p>
+                <p>Menelusuri Jejak Budaya: Eksplorasi Sejarah, Nikmati Karya Seni, dan Temukan Inspirasi di Setiap Sudut Museum.</p>
             </div>
 
              <div class="museum-list-container" id="provinsiContainer">
-                ${provinsis
+                ${provinsiList
                   .map(
                     (provinsi) => `
-                    <div class="museum-category mt-5 mb-3">
-                      <h2>${provinsi}</h2>
-                        <ul class="museum-list ${provinsi
-                          .replace(/\s+/g, "-")
-                          .toLowerCase()}">
-                        </ul>
+                    <div class="text-white text-center mt-5 mb-3">
+                      <h2 class="py-2">Museum Di ${provinsi}</h2>
+                        <ul class="museum-list ${provinsi.replace(
+                          /\s+/g,
+                          "-"
+                        )}"></ul>
 
                         <div class="carousel slide">
-                            <button class="carousel-prev" data-provinsi="${provinsi
-                              .replace(/\s+/g, "-")
-                              .toLowerCase()}">
+                            <button class="carousel-prev" data-provinsi="${provinsi.replace(
+                              /\s+/g,
+                              "-"
+                            )}">
                               <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                               <span class="visually-hidden">Previous</span>
                             </button>
 
-                            <div class="see-all-link btn btn-dark" data-provinsi="${provinsi
-                              .replace(/\s+/g, "-")
-                              .toLowerCase()}">
-                                <a href="#" class="text-decoration-none text-white">Lihat Semua</a>
-                            </div>
+                            <a href="#/provinsi/${provinsi.toLowerCase()}" class="see-all-link text-decoration-none text-white btn btn-dark" data-provinsi="${provinsi.replace(
+                      /\s+/g,
+                      "-"
+                    )}">Lihat Semua</a>
 
-                            <button class="carousel-next" data-provinsi="${provinsi
-                              .replace(/\s+/g, "-")
-                              .toLowerCase()}">
+                            <button class="carousel-next" data-provinsi="${provinsi.replace(
+                              /\s+/g,
+                              "-"
+                            )}">
                               <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                              <span class="visually-hidden">Next</span>  
+                              <span class="visually-hidden">Next</span>
                             </button>
                         </div>
-
                     </div>
                 `
                   )
@@ -84,66 +75,56 @@ const Home = {
 
   async afterRender() {
     try {
-      const provinsis = [
-        "DKI Jakarta",
-        "Jawa Barat",
-        "Jawa Tengah",
-        "Jawa Timur",
-        "Banten",
-      ];
+      const provinsiList = await MuseumSource.provinsiList();
 
-      for (const provinsi of provinsis) {
+      for (const provinsi of provinsiList) {
         const museums = await MuseumSource.museumByProvinsi(provinsi);
+        // console.log(museums);
         const ulProvinsi = document.querySelector(
-          `.museum-list.${provinsi.replace(/\s+/g, "-").toLowerCase()}`
+          `.museum-list.${provinsi.replace(/\s+/g, "-")}`
         );
 
-        const carouselPrev = document.querySelector(
-          `.carousel-prev[data-provinsi="${provinsi
-            .replace(/\s+/g, "-")
-            .toLowerCase()}"]`
-        );
-        const carouselNext = document.querySelector(
-          `.carousel-next[data-provinsi="${provinsi
-            .replace(/\s+/g, "-")
-            .toLowerCase()}"]`
-        );
-
-        // batas jumlah museum yang ditampilkan
         const slicedMuseums = museums.slice(0, 12);
 
         slicedMuseums.forEach((museum) => {
           const museumItem = document.createElement("li");
           museumItem.classList.add("museum-item");
-
           museumItem.innerHTML = createMuseumItemTemplate(museum);
           ulProvinsi.appendChild(museumItem);
 
-          // klik item museum
           const exploreLink = museumItem.querySelector(".card-link");
           exploreLink.addEventListener("click", (event) => {
             event.preventDefault();
-            const museumId = museum.id;
-            window.location.hash = `#/museum/${museumId}`;
+            const museumId = museum.id_museum;
+            // console.log(museumId);
+            window.location.hash = `#/detail/${museumId}`;
           });
         });
 
-        // link "Lihat Semua"
-        const seeAllLink = document.querySelector(
-          `.see-all-link[data-provinsi="${provinsi
-            .replace(/\s+/g, "-")
-            .toLowerCase()}"] a`
+        const carouselPrev = document.querySelector(
+          `.carousel-prev[data-provinsi="${provinsi.replace(/\s+/g, "-")}"]`
         );
-        seeAllLink.href = `/#/provinsi/${provinsi
-          .replace(/\s+/g, "-")
-          .toLowerCase()}`;
+        const carouselNext = document.querySelector(
+          `.carousel-next[data-provinsi="${provinsi.replace(/\s+/g, "-")}"]`
+        );
+        const seeAllLink = document.querySelector(
+          `.see-all-link[data-provinsi="${provinsi.replace(/\s+/g, "-")}"]`
+        );
 
-        // event listener untuk tombol "Previous"
+        if (seeAllLink) {
+          seeAllLink.addEventListener("click", (event) => {
+            event.preventDefault();
+            const targetProvinsi = provinsi.toLowerCase();
+            window.location.hash = `#/provinsi/${targetProvinsi}`;
+            console.log(window.location.hash);
+          });
+        } else {
+          console.error(`Error: seeAllLink not found for provinsi ${provinsi}`);
+        }
+
         carouselPrev.addEventListener("click", () =>
           this.carouselPrevClickHandler(ulProvinsi)
         );
-
-        // event listener untuk tombol "Next"
         carouselNext.addEventListener("click", () =>
           this.carouselNextClickHandler(ulProvinsi, slicedMuseums.length)
         );
@@ -154,14 +135,14 @@ const Home = {
   },
 
   carouselPrevClickHandler(ulProvinsi) {
-    ulProvinsi.scrollLeft -= 300; // jumlah scroll
+    ulProvinsi.scrollLeft -= 300;
   },
 
   carouselNextClickHandler(ulProvinsi, museumCount) {
-    const itemWidth = 300; // lebar item museum
-    ulProvinsi.scrollLeft += itemWidth; // jumlah scroll
+    const itemWidth = 300;
+    ulProvinsi.scrollLeft += itemWidth;
     if (ulProvinsi.scrollLeft >= itemWidth * museumCount) {
-      ulProvinsi.scrollLeft = 0; // Reset ke awal setelah mencapai akhir daftar
+      ulProvinsi.scrollLeft = 0;
     }
   },
 };
