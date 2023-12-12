0f7c98642846ed5b2ab43f022f404994a233d095
Gilangg13 <gilangputra295@gmail.com>
2023-12-12 23:35:35 +0700

    change api url

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-  MUSEUM_URL: "http://localhost:5001",
-  CACHE_NAME: new Date().toISOString(),
+  MUSEUM_URL: "http://localhost:5001/api",
+  CACHE_NAME: "WartaMuseum-v1",
   DATABASE_NAME: "museum-database",
   DATABASE_VERSION: 1,
   OBJECT_STORE_NAME: "museum",
